49f38730ac4d3d11eb5541152b6f5eb1882ab35f
Not_Good <reetujoshi588@gmail.com>
2026-06-24 07:06:12 +0530

    Update panel-setup.js

--- commands/tickets/panel-setup.js
+++ commands/tickets/panel-setup.js
@@ -1,41 +1,25 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+const fs = require('node:fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('panel-setup')
-        .setDescription('Create a custom ticket panel')
-        .addStringOption(option => option.setName('title').setDescription('Embed Title').setRequired(true))
-        .addStringOption(option => option.setName('description').setDescription('Embed Description').setRequired(true))
-        // Category 1
-        .addStringOption(o => o.setName('c1_title').setDescription('Category 1 Title').setRequired(true))
-        .addStringOption(o => o.setName('c1_desc').setDescription('Category 1 Desc').setRequired(true))
-        // Category 2
-        .addStringOption(o => o.setName('c2_title').setDescription('Category 2 Title'))
-        .addStringOption(o => o.setName('c2_desc').setDescription('Category 2 Desc'))
-        // Add more as needed (c3, c4, c5, c6...)
-        ,
+        .setDescription('Create the ticket panel'),
 
     async execute(interaction) {
-        const title = interaction.options.getString('title');
-        const description = interaction.options.getString('description');
-
-        const options = [];
-        for (let i = 1; i <= 2; i++) { // Increase this loop to 6 if you add more options
-            const t = interaction.options.getString(`c${i}_title`);
-            const d = interaction.options.getString(`c${i}_desc`);
-            if (t) options.push({ label: t, value: t.toLowerCase().replace(/\s/g, '_'), description: d || 'Support' });
-        }
+        // Read your JSON file
+        const data = JSON.parse(fs.readFileSync('./tickets.json', 'utf8'));
 
         const menu = new ActionRowBuilder().addComponents(
             new StringSelectMenuBuilder()
                 .setCustomId('ticket_menu')
                 .setPlaceholder('Select a category...')
-                .addOptions(options)
+                .addOptions(data.categories) // This now grabs ALL categories from the JSON
         );
 
         const embed = new EmbedBuilder()
-            .setTitle(title)
-            .setDescription(description)
+            .setTitle("Support Tickets")
+            .setDescription("Select a category below to open a ticket.")
             .setColor(0x00ff00);
 
         await interaction.reply({ embeds: [embed], components: [menu] });
